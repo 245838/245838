367242c801b8643dcc2d884995377feb0e320b6b
Jakub <246339@edu.lodz.pl>
2022-10-24 12:54:43 +0200

    aktualizacja 1

--- main.c
+++ main.c
@@ -1,6 +1,10 @@
 #include <stdio.h>
-
-int main() {
-    printf("Hello, World!\n");
-    return 0;
+int Policz(int a,int b)
+{
+    return a+b;
+}
+int main()
+{
+    printf("Hello World\n");
+    printf("W pierwszym branchu; 2+3=%d", Policz(2,3));
 }
